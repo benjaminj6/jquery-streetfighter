25c808a7ce36acca85cdc56d8c0ea57af0704be9
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-06-29 02:49:48 -0700

    fixed code

--- js/app.js
+++ js/app.js
@@ -26,7 +26,21 @@ $(document).ready(function () {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
-})
+
+	$(document).keydown(function(event) {
+		if ( event.which == 88 ) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		};
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+		};
+	});
+});
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
